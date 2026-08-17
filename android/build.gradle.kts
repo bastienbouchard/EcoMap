3232f3e24d045d7867cb6cfdb34d1e24c73070ff
Bastien Bouchard <bastienbouchard@MBP-de-Bastien.ht.home>
2026-08-17 13:14:26 -0400

    Android: forcer Google Play Billing 8.0.0 (deadline 30 août 2026)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,12 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    configurations.all {
+        resolutionStrategy {
+            force("com.android.billingclient:billing:8.0.0")
+            force("com.android.billingclient:billing-ktx:8.0.0")
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
